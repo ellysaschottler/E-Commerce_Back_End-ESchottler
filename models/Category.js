0a92354841dabef1786adb78477dbd9953cc9598
ellysaschottler <ellysastork@gmail.com>
2023-12-06 09:49:28 -0600

    updated the model properties for validations, more specific datatypes, references to other models

--- models/Category.js
+++ models/Category.js
@@ -6,8 +6,15 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
     category_name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull:false
     }
   },
   {
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,11 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
     product_id: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
     },
     tag_id: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
